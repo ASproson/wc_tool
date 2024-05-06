2e3396971bcc2fe7d5b41af827415f6101d06754
ASproson <a.sproson@outlook.com>
2024-05-06 13:55:13 +0100

    Adds word counter

--- ccwc.py
+++ ccwc.py
@@ -5,17 +5,30 @@ def count_lines(filename):
     line_count = sum(1 for _ in file)
   return line_count
 
+def count_words(filename):
+  word_count = 0
+  with open(filename, 'r') as file:
+    for line in file:
+      words = line.split()
+      word_count += len(words)
+  return word_count
+
 def main():
   parser = argparse.ArgumentParser(prog="ccwc.py", description="Unix word counter", usage="[program] [file] [options]")
   parser.add_argument('filename', help="Input file")
   parser.add_argument("-l", "--lines", action='store_true', help="Count lines in the specified file")
-  args = parser.parse_args()
+  parser.add_argument("-w", "--words", action='store_true', help="Count words in the specified file")
 
+  args = parser.parse_args()
   file = args.filename
 
   if args.lines:
     line_count = count_lines(file)
-    print(f"Number of lines {file}: {line_count}")
+    print(f"Number of lines in {file}: {line_count}")
+
+  if args.words:
+    word_count = count_words(file)
+    print(f"Number of words in {file}: {word_count}")
 
 if __name__ == "__main__":
   main()
